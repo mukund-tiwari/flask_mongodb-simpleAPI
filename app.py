0781a87db6792a6c8dd0ef2f7b98dcadeaf7f028
Mukund Tiwari <mukundtiwari@outlook.com>
2022-01-13 20:37:18 +0530

    enabled CORS

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pymongo
 import json
 from bson import json_util
 
 app = Flask(__name__)
+CORS(app)
 
 client = pymongo.MongoClient('localhost', 27017)
 db = client.testdb
